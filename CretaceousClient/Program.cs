5695da729783ad87f61155ce450e73f315be1294
Brian Hensley <brnhensley@gmail.com>
2021-09-17 10:56:12 -0700

    change port number in launchSettings

--- CretaceousClient/Program.cs
+++ CretaceousClient/Program.cs
@@ -21,7 +21,6 @@ namespace CretaceousClient
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:5004/");
                 });
     }
 }
